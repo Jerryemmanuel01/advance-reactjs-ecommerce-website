1deef18ad16881674d60e770b0582bd313453be1
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-02-01 10:18:28 +0100

    nav added and responsive accross all platforms

---
+++ src/components/NavItems.jsx
@@ -0,0 +1,72 @@
+import {React, useState} from 'react'
+import { Link } from 'react-router-dom'
+import logo from "../assets/images/logo/logo.png"
+
+const NavItems = () => {
+    const [menuToggle, setmenuToggle] = useState(false)
+    const [socialToggle, setsocialToggle] = useState(false)
+    const [headerFixed, setheaderFixed] = useState(false)
+
+    //add event listener
+    window.addEventListener("scroll", () => {
+        if(window.scrollY > 200){
+            setheaderFixed(true)
+        }else{
+            setheaderFixed(false)
+        }
+    })
+  return (
+    <header className={`header-section style-4 ${headerFixed ? 'header-fixed fadeInUp' : ""}`}>
+        {/* header top start */}
+        <div className={`header-top d-md-none ${socialToggle ? 'open' : ''}`}>
+            <div className="container">
+                <div className="header-top-area">
+                    <Link to="/signup" className='lab-btn me-3'><span>Create Account</span></Link>
+                    <Link to="/login"><span>Log in</span></Link>
+                </div>
+            </div>
+        </div>
+
+    {/* header botton */}
+    <div className="header-botton">
+        <div className="container">
+            <div className="header-wrapper">
+                {/* logo */}
+                <div className="logo-search-acte">
+                    <Link to={'/'}>
+                        <img src={logo} alt="" />
+                    </Link>
+                </div>
+
+                {/* menu area */}
+                <div className="menu-area">
+                    <div className="menu">
+                        <ul className={`lab-ul ${menuToggle ? "active" : ""}`}>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/shop'>Shop</Link></li>
+                            <li><Link to='/blog'>Blog</Link></li>
+                            <li><Link to='/about'>About</Link></li>
+                            <li><Link to='/contact'>Contact</Link></li>
+                        </ul>
+                    </div>
+                    
+                    {/* sign in or log in */}
+                    <Link to="/signup" className='lab-btn me-3 d-none d-md-block'>Create Account</Link>
+                    <Link to="/login" className='d-none d-md-block'>Log In</Link>
+
+                    {/* menu toggle */}
+                    <div onClick={() => setmenuToggle(!menuToggle)} className={`header-bar d-lg-none ${menuToggle ? 'active' : ""}`}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </header>
+  )
+}
+
+export default NavItems
