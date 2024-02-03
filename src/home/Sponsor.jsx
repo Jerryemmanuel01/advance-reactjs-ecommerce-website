2b8c2741ca47077ac6ca9743f38cd4cf4e2ec8f5
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-02-03 23:51:51 +0100

    sponsors section completed

--- src/home/Sponsor.jsx
+++ src/home/Sponsor.jsx
@@ -4,6 +4,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
+// import required modules
+import { Autoplay } from 'swiper/modules';
+
 const sponsorList = [
   {
     imgUrl: "/src/assets/images/sponsor/01.png",
@@ -30,7 +33,46 @@ const Sponsor = () => {
     <div className="sponsor-section section-bg">
       <div className="container">
         <div className="section-wrapper">
-          <div className="sponsor-slider"></div>
+          <div className="sponsor-slider">
+            <Swiper
+              slidesPerView={2}
+              spaceBetween={20}
+              autoplay={
+                {
+                    delay:2000,
+                    disableOnInteraction:false
+                }
+              }
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 5,
+                  spaceBetween: 50,
+                },
+              }}
+              modules={[Autoplay]}
+              className="mySwiper"
+            >
+              {
+                sponsorList.map((val, i) => (
+                    <SwiperSlide key={i}>
+                        <div className="sponsor-item">
+                            <div className="sponsor-thumb">
+                                <img src={val.imgUrl} alt="" />
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                ))
+              }
+            </Swiper>
+          </div>
         </div>
       </div>
     </div>
