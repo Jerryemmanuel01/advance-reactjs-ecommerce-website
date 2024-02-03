7a00eed3ab408e1a490672919db512e05a031da9
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-02-03 23:41:15 +0100

    sponsors section created

---
+++ src/home/Sponsor.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+//Import swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const sponsorList = [
+  {
+    imgUrl: "/src/assets/images/sponsor/01.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/02.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/03.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/04.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/05.png",
+  },
+  {
+    imgUrl: "/src/assets/images/sponsor/06.png",
+  },
+];
+
+const Sponsor = () => {
+  return (
+    <div className="sponsor-section section-bg">
+      <div className="container">
+        <div className="section-wrapper">
+          <div className="sponsor-slider"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sponsor;
