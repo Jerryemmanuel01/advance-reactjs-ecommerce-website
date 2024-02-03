c508697720dbac7888ee295c87071789b8d4ce71
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-02-03 14:42:17 +0100

    register section completed

--- src/home/Register.jsx
+++ src/home/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const subTitle = "Save The Day"
 const title = (
-    <h2>Join on Day Long Free Workshop for <b>Advance <span>Mastering</span></b> on Sales</h2>
+    <h2 className='title'>Join on Day Long Free Workshop for <b>Advance <span>Mastering</span></b> on Sales</h2>
 )
 const desc = 'Limited Time Offer! Hurry Up'
 
@@ -23,6 +23,11 @@ const Register = () => {
                         <h4>Register Now</h4>
                         <form className='register-form'>
                             <input type='text' name='name' placeholder='Username' className='reg-input' />
+                            <input type='email' name='email' placeholder='Email' className='reg-input' />
+                            <input type='number' name='number' placeholder='Phone' className='reg-input' />
+                            <button type='submit' className='lab-btn'>
+                                Register Now
+                            </button>
                         </form>
                     </div>
                 </div>
