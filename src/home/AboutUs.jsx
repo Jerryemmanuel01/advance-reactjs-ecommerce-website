e145148a58c675c24cb8fafb2890d761bf70769d
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-02-03 23:04:51 +0100

    about section completed

--- src/home/AboutUs.jsx
+++ src/home/AboutUs.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from "react";
+import CountUp from "react-countup";
+import { Link } from "react-router-dom";
 
 const subTitle = "Why Choose Us";
 const title = "Become a Marchant";
@@ -26,31 +28,50 @@ const countList = [
 
 const AboutUs = () => {
   return (
-    <div className='instructor-section style-2 padding-tb section-bg-ash'>
-        <div className="container">
-            <div className="section-wrapper">
-                <div className="row">
-                    <div className="col">
-                        {
-                            countList.map((val,i) => (
-                                <div key={i} className='count-item'>
-                                    <div className="count-inner">
-                                        <div className="count-icon">
-                                            <i className={val.iconName}></i>
-                                        </div>
-                                        <div className="count-content">
-                                            <h2><span></span></h2>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        }
+    <div className="instructor-section style-2 padding-tb section-bg-ash">
+      <div className="container">
+        <div className="section-wrapper">
+          <div className="row g-4 justify-content-center align-item-center row-cols-1 row-cols-md-2 row-cols-xl-3">
+            <div className="col">
+              {countList.map((val, i) => (
+                <div key={i} className="count-item">
+                  <div className="count-inner">
+                    <div className="count-icon">
+                      <i className={val.iconName}></i>
                     </div>
+                    <div className="count-content">
+                      <h2>
+                        <span className="count">
+                          <CountUp end={val.count} />
+                        </span>
+                        <span>+</span>
+                      </h2>
+                      <p>{val.text}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div className="col">
+                <div className="instructor-content">
+                    <span className="subtitle">{subTitle}</span>
+                    <h2 className="title">{title}</h2>
+                    <p>{desc}</p>
+                    <Link to='/sign-up' className="lab-btn">{btnText}</Link>
                 </div>
             </div>
-        </div>
-    </div>
-  )
-}
 
-export default AboutUs
+            <div className="col">
+                <div className="instructor-thumb">
+                    <img src="/src/assets/images/instructor/01.png" alt="" />
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
