3988d9cff5e8f1061ab988333ff778c7dd00e00f
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-02-07 00:15:09 +0100

    product cart created

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from '../components/PageHeader'
+import Data from "../products.json"
+
+const showResults = 'showing 01 - 12 of 129 Results'
+
 
 const Shop = () => {
+    const [gridList, setgridList] = useState(true)
+    const [product, setproduct] = useState(Data)
   return (
     <div>
-        <PageHeader title="Our Shop Page" curPage="Shop" />
+      <PageHeader title="Our Shop Page" curPage="Shop" />
+      {/* shop page */}
+      <div className="shop-page padding-tb">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                {/* layout and title here */}
+                <div className="shop-title d-flex flex-wrap justify-content-between">
+                  <p>{showResults}</p>
+                    <div className={`product-view-mode ${gridList ? 'gridActive' : 'listActive'}`}>
+                        <a className="grid" onClick={() => setgridList(!gridList)}>
+                            <i className="icofont-ghost"></i>
+                        </a>
+                        <a className="list" onClick={() => setgridList(!gridList)}>
+                            <i className="icofont-listine-dots"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div className=""></div>
+              </article>
+            </div>
+            <div className="col-lg-4 col-12">right side</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Shop
