e61415d1b75778147314d36ef5e4dbe2c70aa63f
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-03-01 23:36:50 +0100

    cartPage completed

--- src/shop/CartPage.jsx
+++ src/shop/CartPage.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import PageHeader from "../components/PageHeader";
+import { Link } from "react-router-dom";
+import delImgUrl from '../assets/images/shop/del.png'
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([]);
@@ -56,7 +59,65 @@ const CartPage = () => {
   //order total
   const orderTotal = cartSubTotal;
 
-  return <div>CartPage</div>;
+  return (
+    <div>
+      <PageHeader title={'Shop Cart'} curPage={"Cart Page"} />
+
+      <div className="shop-cart padding-tb">
+        <div className="container">
+            <div className="section-wrapper">
+                {/* cart top */}
+                <div className="cart-top">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th className="cat-product">Product</th>
+                                <th className="cat-price">Price</th>
+                                <th className="cat-quantity">Quantity</th>
+                                <th className="cat-toprice">Total</th>
+                                <th className="cat-edit">Edit</th>
+                            </tr>
+                        </thead>
+
+                        {/* table body */}
+                        <tbody>
+                            {
+                                cartItems.map((item, i) => (
+                                    <tr key={i}>
+                                        <td className="product-item cat-product">
+                                            <div className="p-thumb">
+                                                <Link to="/shop"><img src={item.img} alt="" /></Link>
+                                            </div>
+                                            <div className="p-content">
+                                                <Link to="/shop">{item.name}</Link>
+                                            </div>
+                                        </td>
+                                        <td className="cat-price">$ {item.price}</td>
+                                        <td className="cat-quantity"> 
+                                            <div className="cart-plus-minus">
+                                                <div className="dec qtybutton" onClick={() => handleDecrease(item)}>-</div>
+                                                <input type="text" className="cart-plus-minus-box" name="qtybutton" value={item.quantity} />
+                                                <div className="inc qtybutton" onClick={() => handleIncrease(item)}>+</div>
+                                            </div>    
+                                        </td>
+                                        <td>$ {calculateTotalPrice(item)} </td>
+                                        <td className="cart-edit">
+                                            <a href="#" onClick={() => handleRemoveItem(item)}>
+                                                <img src={delImgUrl} alt="" />
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
