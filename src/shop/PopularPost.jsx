2fdeeaf6401ef065fdd920d40f19d2f1b5a37be7
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-03-04 19:53:49 +0100

    About component created

--- src/shop/PopularPost.jsx
+++ src/shop/PopularPost.jsx
@@ -45,13 +45,13 @@ const PopularPost = () => {
         {postList.map((blog, i) => (
           <li key={i} className='d-flex justify-content-between'>
             <div className="post-thumb">
-              <Link to={`/blog ${blog.id}`}>
+              <Link to={`/blog/${blog.id}`}>
                 {" "}
                 <img src={blog.imgUrl} alt="" />{" "}
               </Link>
             </div>
             <div className="post-content">
-              <Link to={`/blog ${blog.id}`}> <h5>{blog.title}</h5> </Link>
+              <Link to={`/blog/${blog.id}`}> <h5>{blog.title}</h5> </Link>
               <p>{blog.date}</p>
             </div>
           </li>
