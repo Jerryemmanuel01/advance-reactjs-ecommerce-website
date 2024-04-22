5276e7d88cddf6d544aa0c1cf104f3a38510af83
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-04-22 07:46:48 +0100

    little changes made

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import PageHeader from '../components/PageHeader';
+
+const subTitle = "About Our Brand";
+const title = "Good Qualification Services And Better Expriences";
+const desc =
+  "Distinctively provide acces mutfuncto users whereas transparent proceses somes ncentivize eficient functionalities rather than extensible archtectur communicate leveraged services and cross-platform.";
+
+const year = "30+";
+const expareance = "Years Of Experiences";
+
+const aboutList = [
+  {
+    imgUrl: "/src/assets/images/about/icon/01.jpg",
+    imgAlt: "about icon rajibraj91 rajibraj",
+    title: "Skilled Instructors",
+    desc: "Distinctively provide acces mutfuncto users whereas communicate leveraged services",
+  },
+  {
+    imgUrl: "/src/assets/images/about/icon/02.jpg",
+    imgAlt: "about icon rajibraj91 rajibraj",
+    title: "Get Certificate",
+    desc: "Distinctively provide acces mutfuncto users whereas communicate leveraged services",
+  },
+  {
+    imgUrl: "/src/assets/images/about/icon/03.jpg",
+    imgAlt: "about icon rajibraj91 rajibraj",
+    title: "Online Classes",
+    desc: "Distinctively provide acces mutfuncto users whereas communicate leveraged services",
+  },
+];
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <PageHeader title={"About Our Brand"} curPage={"About"} />
+
+        <div className="about-section style-3 padding-tb section-bg">
+            
+        </div>
+    </div>
   )
 }
 
