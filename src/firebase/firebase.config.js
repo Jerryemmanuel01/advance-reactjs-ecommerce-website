390e4f8292181c9be5ac4fe874dba95d3e6ed931
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-04-23 18:41:14 +0100

    enviroment variables added for firebase config file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB9mWUTR3lIDO7fRCf5kKug3LZMOXscapg",
-  authDomain: "react-ecommerce-app-2c146.firebaseapp.com",
-  projectId: "react-ecommerce-app-2c146",
-  storageBucket: "react-ecommerce-app-2c146.appspot.com",
-  messagingSenderId: "751796813718",
-  appId: "1:751796813718:web:8cdf9a5aab81b7207d27b7",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
